11d48af1ad5ca83322f786907eedc4a3f4ebd917
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-05 14:35:07 +0530

    Iterate Through an Array with a For Loop

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-const myArray = [];
-
-for(var i=9; i>0; i-=2){
-  myArray.push(i);
+const myArr = [2, 3, 4, 5, 6];
+var total=0;
+for (var i = 0; i <= myArr.length - 1; i++) {
+  total += myArr[i]
 }
 // Only change code below this line
