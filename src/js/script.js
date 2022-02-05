052a6ee8f6b7a7cc7d4678f2ec4f8d1e84f7e4a2
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-05 13:26:27 +0530

    Counting Cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,25 @@
-// Setup
-function abTest(a, b) {
-    // Only change code below this line
-  if(a < 0 || b < 0){
-    return;
-  }
-  
-  
-    // Only change code above this line
-  
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-  }
-  
-  abTest(2,2);
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+if(card >= 2 && card <=6){
+  count++;
+}
+else if( card >= 7 && card <= 9){}
+else if( card === 10 || card === 'J' ||card ==='Q' ||card ==="K" ||card ==="A" ){
+  count--;
+}
+
+if(count >0){
+  return count +" Bet";
+}
+else if(count <=0){
+  return count +" Hold";
+}
+else{
+  return "Change Me";
+}
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
