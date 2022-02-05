ea8d27f2338673d05d884800a5585a0ae3e4bb40
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-05 11:19:47 +0530

    Constructing Strings with Variables

--- src/js/script.js
+++ src/js/script.js
@@ -90,3 +90,6 @@ const myStr = "This is the start."+" This is the end.";
 
 let myStr= "This is the first sentence. "
 myStr += "This is the second sentence.";
+
+const myName = "sarvesh";
+const myStr = "My name is "+myName+" and and I am well!";
