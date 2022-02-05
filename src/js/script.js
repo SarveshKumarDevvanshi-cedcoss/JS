ae05b9fb0625b3c05df0ffa9b106d63c457c6a83
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-05 13:02:16 +0530

    Golf Code

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,18 @@
-function testSize(num) {
-    // Only change code below this line
-   if(num < 5){ return "Tiny";}
-   else if(num < 10){ return "Small";}
-  else if(num < 15){ return "Medium";}
-   else if (num < 20){ return "Large";}
-  else if (num >= 20) {return "Huge";}
-  
-    else {return "Change Me";}
-    // Only change code above this line
-  }
-  
-  testSize(20);
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if(strokes === 1){ return "Hole-in-one!";}
+else if(strokes <= par-2){ return "Eagle";}
+else if(strokes === par-1){return "Birdie";}
+else if (strokes === par) {return "Par";}
+else if (strokes === par +1 ){ return "Bogey";}
+else if(strokes === par+2) {return "Double Bogey";}
+else if( strokes >= par+3){return "Go Home!";}
+else{
+  return "Change Me";
+}
+  // Only change code above this line
+}
+
+golfScore(5, 4);
