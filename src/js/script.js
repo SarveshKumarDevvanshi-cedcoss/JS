ced7ecc147a82b800f9ba32a69f4d8a508fdcea9
SarveshKumarDevvanshi-cedcoss <sarveshkumardevanshi@cedcoss.com>
2022-02-05 10:32:13 +0530

    Understanding Uninitialized Variables

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,12 @@ multi-line
  var a=9;
  var myFirstName="sarvesh";
 var myLastName="Kumar";
+
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
